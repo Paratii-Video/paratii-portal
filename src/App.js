1ec3c88e32dc4d6e0fa3c9b20cb3535e5ef3b6c9
Elia Pellegrino <elia@piramid.studio>
2017-12-21 17:57:44 +0100

    added correct logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './paratii_logo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Paratii Portal!!</h1>
+          <h1 className="App-title">Paratii Portal</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Everything starts here..
         </p>
       </div>
     );
