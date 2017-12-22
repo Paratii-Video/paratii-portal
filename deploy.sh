c56733823f5334067baacc6b1c378b46952098f5
Jelle Gerbrandy <github@gerbrandy.com>
2017-12-22 10:03:13 +0100

    continuous deployment: attempt n. 1

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# the 'build_and_deploy.sh' referred to in this script is https://github.com/Paratii-Video/devops/blob/master/files/build_and_deploy.sh
+
+if [[ $1 == staging ]]; then
+    # ok
+    echo "deploying to $1"
+elif [[ $1 == production ]]; then
+    # ok
+    echo "deploying to $1"
+else
+    echo "unknown parameter - please specify one of 'staging' or 'production'"
+    exit
+fi
+
+ssh paratii@build.paratii.video "cd ~/paratii-devops && fab deploy_portal $1 </dev/null >deploy_portal.log 2>&1 &"
