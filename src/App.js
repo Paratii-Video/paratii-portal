d56d82f1bd66bb53ac456859446626f0fb9d73fc
Jelle Gerbrandy <github@gerbrandy.com>
2017-12-22 10:45:14 +0100

    test CD

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Paratii Portal</h1>
         </header>
         <p className="App-intro">
-          Everything starts here..
+          Everything starts here.
         </p>
       </div>
     );
