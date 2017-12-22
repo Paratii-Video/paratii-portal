cf1bfec66d80c41ce644dbc11ed2c76b6db32d07
Benjamin Baum <bjb253@cornell.edu>
2017-12-22 16:52:46 -0500

    7 - comment out redux devtools by default

--- src/scripts/createStore.js
+++ src/scripts/createStore.js
@@ -3,7 +3,7 @@ import thunk from "redux-thunk";
 
 import reducer from "reducers/index";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = /* window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || */ compose;
 
 export default () => {
   const store = createStore(
