7d821c21169e49519843f7ae932e54ea10dd0f0d
Benjamin Baum <bjb253@cornell.edu>
2017-12-26 10:53:37 -0500

    16 - actually add play container

---
+++ src/scripts/containers/PlayContainer.js
@@ -0,0 +1,9 @@
+import { connect } from "react-redux";
+
+import Play from "components/Play";
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = () => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Play);
