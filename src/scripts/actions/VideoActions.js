b76988e2386fa07094fa008d389d4452dfcdc1ca
Benjamin Baum <bjb253@cornell.edu>
2017-12-26 17:34:30 -0500

    16 - more attempts to fetch video info

--- src/scripts/actions/VideoActions.js
+++ src/scripts/actions/VideoActions.js
@@ -1,14 +1,27 @@
 /* @flow */
 
 import { createAction } from "redux-actions";
-import Paratii from "paratii-lib";
-
-const paratii = new Paratii();
+import { ParatiiEth } from "paratii-lib/dist/bundle";
 
 import { SET_VIDEO_ID } from "constants/ActionConstants";
+
+const eth = new ParatiiEth({
+  provider: "http://localhost:8545",
+  registryAddress: "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1",
+  address: "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1",
+  privateKey:
+    "0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709"
+});
 
 export const setVideoId = createAction(SET_VIDEO_ID);
 
 export const fetchVideo = (id: string) => (getState, dispatch) => {
-  console.log(paratii.eth.vids.get(id));
+  eth.vids
+    .get(id)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(e => {
+      console.log("e: ", e);
+    });
 };
