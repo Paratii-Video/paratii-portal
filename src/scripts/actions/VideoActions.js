d69ab9c9ffde8353cd1de8449b784197ba29ad21
Benjamin Baum <bjb253@cornell.edu>
2017-12-27 09:08:10 -0500

    16 - video actions poc

--- src/scripts/actions/VideoActions.js
+++ src/scripts/actions/VideoActions.js
@@ -1,35 +1,48 @@
 /* @flow */
 
 import { createAction } from "redux-actions";
-import { ParatiiEth } from "paratii-lib/dist/bundle";
+import { Paratii } from "paratii-lib/dist/bundle";
 
 import { SET_VIDEO_ID } from "constants/ActionConstants";
 
-const eth = new ParatiiEth({
-  provider: "http://localhost:8545",
-  registryAddress: "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1",
-  address: "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1",
-  privateKey:
-    "0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709"
+const paratii = new Paratii({
+  provider: "http://localhost:8545"
 });
+
+paratii.core.vids
+  .create({
+    id: "0x90f8bf6a479",
+    // ipfsHash: "QmNZS5J3LS1tMEVEP3tz3jyd2LXUEjkYJHyWSuwUvHDaRJ",
+    owner: "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1"
+  })
+  .then(() => {
+    paratii.core.vids
+      .get("0x90f8bf6a479")
+      .then(res => {
+        console.log(res);
+      })
+      .catch(e => {
+        console.log("e: ", e);
+      });
+  });
 
 export const setVideoId = createAction(SET_VIDEO_ID);
 
 export const fetchVideo = (id: string) => (getState, dispatch) => {
-  eth.vids
-    .create({
-      id: "0x90f8bf6a479",
-      ipfsHash: "QmNZS5J3LS1tMEVEP3tz3jyd2LXUEjkYJHyWSuwUvHDaRJ",
-      owner: "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1"
-    })
-    .then(() => {
-      eth.vids
-        .get("0x90f8bf6a479")
-        .then(res => {
-          console.log(res);
-        })
-        .catch(e => {
-          console.log("e: ", e);
-        });
-    });
+  // paratii.core.vids
+  //   .create({
+  //     id: "0x90f8bf6a479",
+  //     // ipfsHash: "QmNZS5J3LS1tMEVEP3tz3jyd2LXUEjkYJHyWSuwUvHDaRJ",
+  //     owner: "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1"
+  //   })
+  //   .then(() => {
+  //     paratii.core.vids
+  //       .get("0x90f8bf6a479")
+  //       .then(res => {
+  //         console.log(res);
+  //       })
+  //       .catch(e => {
+  //         console.log("e: ", e);
+  //       });
+  //   });
 };
