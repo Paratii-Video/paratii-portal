f28ab2650a64fd503ff21d428ae0e2e75082186e
enrico.cicconi <enrico.cicconi@peertribe.com>
2017-12-28 19:22:47 +0100

    adding parity script

---
+++ src/scripts/containers/DebugContainer.js
@@ -0,0 +1,9 @@
+import { connect } from 'react-redux'
+
+import Debug from 'components/Debug'
+
+const mapStateToProps = () => ({})
+
+const mapDispatchToProps = () => ({})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Debug)
