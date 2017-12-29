8d2bd978038e8ffde06a2c57e6988d986fdb5a19
Benjamin Baum <bjb253@cornell.edu>
2017-12-29 16:05:11 -0500

    16 - configure parity further

--- src/scripts/utils/ParatiiLib.js
+++ src/scripts/utils/ParatiiLib.js
@@ -4,9 +4,13 @@ import { Paratii } from 'paratii-lib/lib/paratii'
 
 let instance
 
-export default config => {
+export default (config = {}) => {
   if (!instance) {
-    instance = new Paratii(config)
+    instance = new Paratii({
+      ...config,
+      registryAddress: '0x9e2d04eef5b16CFfB4328Ddd027B55736407B275',
+      privateKey: '399b141d0cc2b863b2f514ffe53edc6afc9416d5899da4d9bd2350074c38f1c6'
+    })
   }
 
   return instance
