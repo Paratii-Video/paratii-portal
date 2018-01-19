f5bb650d2d3796c251e9d1ab7242bf50e2d25236
Benjamin Baum <bjb253@cornell.edu>
2018-01-19 00:24:54 -0600

    Fix client-side routing

--- src/scripts/embed/index.js
+++ src/scripts/embed/index.js
@@ -5,10 +5,9 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
-import { initParatiiLib, paratii } from 'utils/ParatiiLib'
+import { initParatiiLib } from 'utils/ParatiiLib'
 import { getRoot } from 'utils/AppUtils'
 import PlayContainer from 'containers/PlayContainer'
-import Video from 'records/VideoRecords'
 import createStore from 'scripts/createStore'
 
 import 'styles/embed/index.scss'
@@ -19,44 +18,14 @@ type Props = {
   match: RouteMatch
 };
 
-type State = {
-  video?: Video
-};
-
 const store = createStore()
 
-class EmbedApp extends React.Component<Props, State> {
-  fetchVideo: () => void;
-
-  constructor (props: Props) {
-    super(props)
-
-    this.state = {}
-
-    this.fetchVideo = this.fetchVideo.bind(this)
-  }
-
-  fetchVideo (id: string) {
-    paratii().eth.vids.get(id)
-      .then(videoInfo => {
-        this.setState(prevState => {
-          if (!prevState.video) {
-            return {
-              video: new Video(videoInfo)
-            }
-          }
-        })
-      })
-  }
-
+class EmbedApp extends React.Component<Props, void> {
   render () {
     const { match } = this.props
-    const { video } = this.state
-
-    console.log('video: ', video)
 
     return <Route
-      exact path={`${match.url}video/:id`}
+      exact path={`${match.url}embed/:id`}
       component={PlayContainer}
     />
   }
