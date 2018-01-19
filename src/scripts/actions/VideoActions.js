4914e70ef443d7469371a71d65fc87b9dfbfc92b
Benjamin Baum <bjb253@cornell.edu>
2018-01-19 00:22:35 -0600

    A bit more refactoring/cleanup

--- src/scripts/actions/VideoActions.js
+++ src/scripts/actions/VideoActions.js
@@ -2,6 +2,7 @@
 
 import { createAction } from 'redux-actions'
 
+import { paratii } from 'utils/ParatiiLib'
 import { VIDEO_DATA_LOADED } from 'constants/ActionConstants'
 
 import type { Dispatch } from 'redux'
@@ -10,7 +11,7 @@ export const videoDataLoaded = createAction(VIDEO_DATA_LOADED)
 
 export const fetchVideo = (id: string) => async (dispatch: Dispatch<*>) => {
   console.log('fetching video info for video with id: ' + id)
-  let videoInfo = await window.paratii.eth.vids.get(id)
+  let videoInfo = await paratii().eth.vids.get(id)
   // TODO: previous line should be replaced with next line once the db is updated
   // let videoInfo = await paratii.core.vids.get(id)
   console.log(`Received video info`)
