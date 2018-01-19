66950f961e64595914e1d196dbfdcc58f41a666f
Elia Pellegrino <elia@piramid.studio>
2018-01-19 14:40:39 +0100

    added component for header

---
+++ src/scripts/components/MainHeader.js
@@ -0,0 +1,33 @@
+/* @flow */
+
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import logo from 'assets/img/paratii_logo.png'
+import Wrapper from './foundations/Wrapper'
+
+const Header = styled.header`
+  background-color: #222;
+  flex: 0 0 50px;
+  padding: 20px;
+  color: white;
+  display: flex;
+  align-items: center;
+`
+
+const Logo = styled.img`
+  height: 80px;
+`
+
+class MainHeader extends Component {
+  render () {
+    return (
+      <Wrapper>
+        <Header>
+          <Logo src={logo} alt='logo' />
+        </Header>
+      </Wrapper>
+    )
+  }
+}
+
+export default MainHeader
