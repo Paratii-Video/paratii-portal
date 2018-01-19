a220622715a0d73384f2462dcbd5c9b547704444
Benjamin Baum <bjb253@cornell.edu>
2018-01-19 00:15:16 -0600

    Init paratii lib before rendering app

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 
-import { Paratii } from 'paratii-lib/lib/paratii'
+import { initParatiiLib } from 'utils/ParatiiLib'
 import Root from 'components/Root'
 import { getRoot } from 'utils/AppUtils'
 import createStore from 'scripts/createStore'
@@ -13,7 +13,7 @@ import 'styles/app.scss'
 
 const store = createStore()
 
-const renderApp = () => {
+initParatiiLib().then(() => {
   ReactDOM.render(
     <Provider store={store}>
       <AppContainer>
@@ -22,24 +22,7 @@ const renderApp = () => {
     </Provider>,
     getRoot()
   )
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  window.paratii = new Paratii({
-    provider: 'http://localhost:8545/rpc/',
-    address: '0x9e2d04eef5b16CFfB4328Ddd027B55736407B275',
-    privateKey: '399b141d0cc2b863b2f514ffe53edc6afc9416d5899da4d9bd2350074c38f1c6'
-  })
-  window.paratii.eth.deployContracts()
-    .then(renderApp)
-} else {
-  window.paratii = new Paratii({
-    provider: 'http://localhost:8545/rpc/',
-    address: '0x9e2d04eef5b16CFfB4328Ddd027B55736407B275',
-    privateKey: '399b141d0cc2b863b2f514ffe53edc6afc9416d5899da4d9bd2350074c38f1c6'
-  })
-  renderApp()
-}
+})
 
 if (module.hot) {
   module.hot.accept('components/Root', () => {
