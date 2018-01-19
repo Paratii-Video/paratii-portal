fdc6f70525d13da6ed8590d1476db4dfc21c8e29
Jelle Gerbrandy <github@gerbrandy.com>
2018-01-19 14:20:49 +0100

    hardcode registry address

--- src/scripts/utils/ParatiiLib.js
+++ src/scripts/utils/ParatiiLib.js
@@ -21,11 +21,11 @@ if (process.env.NODE_ENV === 'test') {
 } else {
   window.paratii = new Paratii({
     // ...config,
-    provider: 'http://localhost:8545/rpc/'
+    provider: 'http://localhost:8545/rpc/',
     // this is the user's address, and must be set from the browser's wallet
     // address: '0x9e2d04eef5b16CFfB4328Ddd027B55736407B275',
     // privateKey: '399b141d0cc2b863b2f514ffe53edc6afc9416d5899da4d9bd2350074c38f1c6'
-    // registry: '0x0'
+    registryAddress: '0x0B101ff870F8BAd6c437C45eCb2964D7e8034593'
   })
 }
 
