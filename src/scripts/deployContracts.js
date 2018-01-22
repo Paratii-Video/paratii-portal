a8c796801802726a98746ad390e373138448d51a
Jelle Gerbrandy <github@gerbrandy.com>
2018-01-22 12:53:45 +0100

    bump paratii-lib version that had a bugi, add diagno script

--- src/scripts/deployContracts.js
+++ src/scripts/deployContracts.js
@@ -9,6 +9,8 @@ const configFilename = path.join(__dirname, `/../../config/${env}.json`)
 const config = require(configFilename)
 
 const paratii = new Paratii(config)
+let newConfig
+
 function deployContracts () {
   paratii.eth.deployContracts()
     .then(
@@ -16,15 +18,23 @@ function deployContracts () {
     )
     .then(
       registryAddress => {
-        const newConfig = {
+        console.log(`new registry address: ${registryAddress}`)
+        newConfig = {
           ...config,
           registryAddress
         }
         fs.writeFileSync(configFilename, JSON.stringify(newConfig, null, 2), 'utf-8')
         let msg = `written new configuation to ${configFilename}`
         console.log(msg)
+
         return msg
       }
+    )
+    .then(
+      () => paratii.diagnose()
+    )
+    .then(
+      (diagnosis) => console.log(diagnosis)
     )
 }
 deployContracts()
