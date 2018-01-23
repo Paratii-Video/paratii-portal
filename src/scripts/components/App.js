d2ba65a9b2412d2f912a7a07d5bb125303ed9c0a
jrgarou <jrgarou@gmail.com>
2018-01-23 17:08:50 -0200

    body background color

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -37,6 +37,7 @@ const paratiiTheme = {
 const Wrapper = styled.div`
   background-color: ${props => props.theme.colors.body.background};
   font-family: ${props => props.theme.fonts.family ? props.theme.fonts.family : 'Monospace'}, sans-serif;
+  height: 100%;
 `
 //
 
