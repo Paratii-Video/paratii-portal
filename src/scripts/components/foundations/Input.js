1f60eb317fd806327bc635d27ab0f0e342a149b2
Hugo Mitsumori <hugo.mitsumori@hotmail.com.br>
2018-01-23 01:01:20 -0200

    start beautify UploadFile component

--- src/scripts/components/foundations/Input.js
+++ src/scripts/components/foundations/Input.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
 
 export default styled.input`
-  height: 35px;
+  height: 25px;
   font-size: 14px;
   font-weight: 500;
   display: block;
+  border: none;
+  border-bottom: 2px solid #ddd;
+  background-color: rgba(0, 0, 0, 0);
 `
