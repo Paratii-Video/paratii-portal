e17c01776d2829404234607883d841b554ac717a
Jelle Gerbrandy <github@gerbrandy.com>
2018-01-30 21:18:44 +0100

    uncomment commented selectors

--- src/scripts/selectors/UploadSelectors.js
+++ src/scripts/selectors/UploadSelectors.js
@@ -1,48 +1,45 @@
-// import { createSelector } from 'reselect'
+import { createSelector } from 'reselect'
 
-// import { getUpload } from 'selectors/index'
+import { getUpload } from 'selectors/index'
 
 /* Upload */
-// export const getUploadProgress = createSelector(
-//   [getUpload], (upload) => {
-//     return upload.getIn(['uploadStatus', 'data', 'progress']) || 0
-//   }
-// )
+export const getUploadProgress = createSelector([getUpload], upload => {
+  return upload.getIn(['uploadStatus', 'data', 'progress']) || 0
+})
 
-// export const getIsUploading = createSelector(
-//   [getUpload], (upload) => upload.getIn(['uploadStatus', 'name']) === 'running'
-// )
+export const getIsUploading = createSelector(
+  [getUpload],
+  upload => upload.getIn(['uploadStatus', 'name']) === 'running'
+)
 
-// export const getHasVideoInfo = createSelector(
-//   [getUpload], (upload) => !!(upload.getIn(['videoInfo', 'title']))
-// )
+export const getHasVideoInfo = createSelector(
+  [getUpload],
+  upload => !!upload.getIn(['videoInfo', 'title'])
+)
 
-// export const getShowUploadFile = createSelector(
-//   [getUpload], (upload) => {
-//     return upload.getIn(['uploadStatus', 'name']) === 'idle'
-//   }
-// )
+export const getShowUploadFile = createSelector([getUpload], upload => {
+  return upload.getIn(['uploadStatus', 'name']) === 'idle'
+})
 
-// export const getShowEditVideoInfo = createSelector(
-//   [getUpload, getHasVideoInfo], (upload, hasVideoInfo) => {
-//     const uploadStatus = upload.getIn(['uploadStatus', 'name'])
-//     return (uploadStatus !== 'idle' && !hasVideoInfo)
-//   }
-// )
+export const getShowEditVideoInfo = createSelector(
+  [getUpload, getHasVideoInfo],
+  (upload, hasVideoInfo) => {
+    const uploadStatus = upload.getIn(['uploadStatus', 'name'])
+    return uploadStatus !== 'idle' && !hasVideoInfo
+  }
+)
 
-// export const getShowVideoInfo = createSelector(
-//   [getUpload, getHasVideoInfo], (upload, hasVideoInfo) => {
-//     const uploadStatus = upload.getIn(['uploadStatus', 'name'])
-//     return (uploadStatus !== 'idle' && hasVideoInfo)
-//   }
-// )
+export const getShowVideoInfo = createSelector(
+  [getUpload, getHasVideoInfo],
+  (upload, hasVideoInfo) => {
+    const uploadStatus = upload.getIn(['uploadStatus', 'name'])
+    return uploadStatus !== 'idle' && hasVideoInfo
+  }
+)
 
-// export const getIpfsHash = createSelector(
-//   [getUpload], (upload) => {
-//     const uploadStatus = upload.getIn(['uploadStatus', 'name'])
-//     return (uploadStatus === 'success'
-//       ? upload.getIn(['uploadStatus', 'data', 'ipfsHash'])
-//       : ''
-//     )
-//   }
-// )
+export const getIpfsHash = createSelector([getUpload], upload => {
+  const uploadStatus = upload.getIn(['uploadStatus', 'name'])
+  return uploadStatus === 'success'
+    ? upload.getIn(['uploadStatus', 'data', 'ipfsHash'])
+    : ''
+})
