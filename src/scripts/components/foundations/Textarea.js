b17e40318a9074ac639c865d9dd35c114483837a
Elia Pellegrino <elia@piramid.studio>
2018-02-06 15:39:20 +0100

    add textarea in foundation

---
+++ src/scripts/components/foundations/Textarea.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export default styled.textarea`
+  min-height: 100px;
+  font-size: 14px;
+  font-weight: 500;
+  display: block;
+  border: none;
+  border-bottom: 2px solid #ddd;
+  background-color: rgba(0, 0, 0, 0);
+  margin-bottom: 15px;
+`
