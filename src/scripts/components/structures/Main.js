078da5de44d559eb37133b881a807b0d9bdd5c7f
Benjamin Baum <bjb253@cornell.edu>
2018-02-08 17:58:33 -0500

    Constrain height of main component and fix player height

--- src/scripts/components/structures/Main.js
+++ src/scripts/components/structures/Main.js
@@ -9,6 +9,9 @@ const MainWrapper = styled.main`
   background-color: ${props => props.theme.colors.body.background};
   flex: 1 1 100%;
   padding: 80px;
+  display: flex;
+  flex-direction: column;
+  min-height: 500px;
 `
 
 class Main extends Component<Props, void> {
