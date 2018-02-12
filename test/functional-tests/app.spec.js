061c7e6f9783763ea3c6a40741e313fb1fa66b70
Benjamin Baum <bjb253@cornell.edu>
2018-02-12 00:18:44 -0500

    22 - improve page title test reliability

--- test/functional-tests/app.spec.js
+++ test/functional-tests/app.spec.js
@@ -1,8 +1,8 @@
-import { expect } from 'chai'
-
 describe('Page title', function () {
   it('should be Paratii', function () {
     browser.url('http://localhost:8080')
-    expect(browser.getTitle()).to.equal('Paratii')
+    browser.waitUntil(() => {
+      return browser.getTitle() === 'Paratii'
+    })
   })
 })
