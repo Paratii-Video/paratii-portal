24be9d3b5df20bd20aaaf2887df680e96174b727
jrgarou <jrgarou@gmail.com>
2018-02-16 10:36:20 -0200

    Unexpected duplicate name sans-serif

--- src/scripts/components/templates/MainTemplate.js
+++ src/scripts/components/templates/MainTemplate.js
@@ -10,7 +10,7 @@ const Wrapper = styled.div`
   background-color: ${props => props.theme.colors.body.background};
   font-family: ${props =>
     props.theme.fonts.family ? props.theme.fonts.family : 'Monospace'},
-    sans-serif, sans-serif;
+    sans-serif;
   display: flex;
   min-height: 100vh;
   flex-direction: column;
