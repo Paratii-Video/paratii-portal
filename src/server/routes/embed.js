1565ad54f6631ebbb64ef83fb8c438b72194dcec
Jelle Gerbrandy <github@gerbrandy.com>
2018-02-23 02:48:48 -0100

    get real data in embed headers (step 1)

--- src/server/routes/embed.js
+++ src/server/routes/embed.js
@@ -2,28 +2,44 @@
 
 import type { $Request, $Response } from 'express'
 
+// import paratii from 'utils/ParatiiLib'
+
+import { Paratii } from 'paratii-lib/dist/paratii'
+import { getParatiiConfig } from 'utils/AppUtils'
+// import type { ParatiiLib } from 'types/ApplicationTypes'
+
+const paratiiConfig = getParatiiConfig(process.env.NODE_ENV)
+
+const paratii = new Paratii(paratiiConfig)
+
+export default paratii
+
 module.exports = (req: $Request, res: $Response) => {
   const { id } = req.params
-  const title = 'Great title'
-  const description = 'Cool video, please watch it.'
+  // const video = {}
+  // video.title = 'Great title'
+  // video.description = 'Cool video, please watch it.'
+  const video = paratii.core.vids.get(id)
+  // TODO: we need a way to get the ipfs hash of a thumbnail. These should be saved inparatii-db
   const thumbnailUrl =
     'http://paratii.video/imagens/cropped-logo_colorido_horizontal.png'
   const url = `https://portal.paratii.video/play/${id}`
   const embedUrl = `https://portal.paratii.video/embed/${id}`
   const height = `1080`
   const width = `1920`
+  // this needs to ba a video - just as the thumbnail, we need to save these data from paratii-db
   const ipfSource = `https://gateway.paratii.video/ipfs/QmSs64S5J8C9H6ZFYR44YGEB6pLq2SRLYe3MZdUoyNX7EH`
 
   res.send(`
     <!DOCTYPE html>
     <html>
       <head>
-        <title>${title}</title>
+        <title>${video.title}</title>
         <link rel="stylesheet" type="text/css" href="/embed/index.css">
 
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta property="twitter:card" content="player" />
-        <meta property="twitter:title" content="${title}" />
+        <meta property="twitter:title" content="${video.title}" />
         <meta property="twitter:site" content="${url}">
         <meta property="twitter:player:width" content="490" />
         <meta property="twitter:player:height" content="280" />
@@ -37,9 +53,9 @@ module.exports = (req: $Request, res: $Response) => {
         <meta property="og:video:height" content="${height}" />
         <meta property="og:type" content="video.other" />
         <meta property="og:url" content="${url}" />
-        <meta property="og:title" content="${title}" />
+        <meta property="og:title" content="${video.title}" />
         <meta property="og:image" content="${thumbnailUrl}" />
-        <meta property="og:description" content="${description}" />
+        <meta property="og:description" content="${video.description}" />
       </head>
       <body>
         <div id="root"></div>
