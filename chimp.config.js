45045cb43482be47239e586893f83611cb0766b7
Leandro Barbosa <leandrobar93@gmail.com>
2018-02-27 14:27:24 -0300

    update chimp configurations

--- chimp.config.js
+++ chimp.config.js
@@ -4,6 +4,12 @@ module.exports = {
   mocha: true,
   chai: true,
   webdriverio: {
-    waitforTimeout: 20000
+    waitforTimeout: 70000,
+    desiredCapabilities: {
+      chromeOptions: {
+        args: ['headless', 'disable-gpu']
+      },
+      isHeadless: true
+    }
   }
 }
