653bc3e21e497a7e16ddb7a9c22f3c829af84a90
Benjamin Baum <bjb253@cornell.edu>
2018-02-28 22:57:50 -0500

    mediaplayer-37 - fix routes

--- src/server/app.js
+++ src/server/app.js
@@ -30,7 +30,6 @@ if (process.env.NODE_ENV === 'production-notugly') {
 
 app.use(express.static(path.resolve(__dirname, '../../', 'build')))
 app.get('/embed/:id', videoRoute)
-app.get('/play/:id', videoRoute)
 app.get('/oembed', oembedRoute)
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../', 'build', 'index.html'))
