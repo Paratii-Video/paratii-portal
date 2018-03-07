215ede0980bb3983cdfc0beb48cd3408ffde06b5
Benjamin Baum <bjb253@cornell.edu>
2018-03-07 18:24:33 -0500

    mediaplayer - 37 remove dummy title

--- src/scripts/components/VideoOverlay.js
+++ src/scripts/components/VideoOverlay.js
@@ -175,14 +175,7 @@ class VideoOverlay extends Component<Props, State> {
   getVideoTitle (): string {
     const { video } = this.props
 
-    const longSentence =
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-
-    return (
-      longSentence ||
-      (video && (video.get('title') || video.get('filename'))) ||
-      ''
-    )
+    return (video && (video.get('title') || video.get('filename'))) || ''
   }
 
   onProfileButtonClick = (e: Object): void => {
