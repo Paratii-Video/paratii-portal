729f8c96eee99aa9f70dc9856c90ad55b713cfa6
enrico.cicconi <enrico.cicconi@peertribe.com>
2018-03-08 16:27:37 -0300

    handlebars and minor fixes

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,17 @@
+import { Paratii } from 'paratii-lib/dist/paratii'
+import { getParatiiConfig } from 'utils/AppUtils'
+
 const express = require('express')
+const exphbs = require('express-handlebars')
 const devMiddleware = require('webpack-dev-middleware')
 const hotMiddleware = require('webpack-hot-middleware')
 const webpack = require('webpack')
 const webpackConfig = require('../../webpack.config.js')
+const path = require('path')
 const routeHelper = require('./routes/')
 const oembedRoute = require('./routes/oembed')
-
+const paratiiConfig = getParatiiConfig(process.env.NODE_ENV)
+const paratii = new Paratii(paratiiConfig)
 const app = express()
 
 if (process.env.NODE_ENV === 'development') {
@@ -27,15 +33,84 @@ if (process.env.NODE_ENV === 'production-notugly') {
   )
 }
 
-// app.use(express.static(path.resolve(__dirname, '../../', 'build')))
-app.get('/embed/:id', routeHelper)
-app.get('/play/:id', routeHelper)
-app.get('/embed/:id', routeHelper)
-app.get('/', routeHelper)
+const hbs = exphbs.create({
+  // Specify helpers which are only registered on this instance.
+  extname: '.hbs',
+  helpers: {
+    foo: function () {
+      return 'FOO!'
+    },
+    bar: function () {
+      return 'BAR!'
+    }
+  }
+})
+
+app.engine('.hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'src/views')
+app.use(express.static(path.resolve(__dirname, '../../', 'build')))
+
+app.get('/embed/:id', async function (req, res, next) {
+  const { id } = req.params
+  const video = await paratii.core.vids.get(id)
+  let meta = ''
+
+  if (video !== null) {
+    meta = routeHelper.openGraphHead(meta, video)
+    meta = routeHelper.twitterCardHead(meta, video)
+  } else {
+    meta = routeHelper.notFoundVideo(meta)
+  }
+  res.render('index', {
+    helpers: {
+      meta: function () {
+        return meta
+      },
+      script: function () {
+        return '<script type="text/javascript" src="/embed/bundle.js"></script>'
+      }
+    }
+  })
+})
+
+app.get('/play/:id', async function (req, res, next) {
+  const { id } = req.params
+  const video = await paratii.core.vids.get(id)
+  let meta = ''
+
+  if (video !== null) {
+    meta = routeHelper.openGraphHead(meta, video)
+    meta = routeHelper.twitterCardHead(meta, video)
+  } else {
+    meta = routeHelper.notFoundVideo(meta)
+  }
+  res.render('index', {
+    helpers: {
+      meta: function () {
+        return meta
+      },
+      script: function () {
+        return '<script type="text/javascript" src="/bundle.js"></script>'
+      }
+    }
+  })
+})
+
+app.get('*', function (req, res, next) {
+  const meta = routeHelper.basicHead('')
+  res.render('index', {
+    helpers: {
+      meta: function () {
+        return meta
+      },
+      script: function () {
+        return '<script type="text/javascript" src="/bundle.js"></script>'
+      }
+    }
+  })
+})
+
 app.get('/oembed', oembedRoute)
-app.get('*', routeHelper)
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../../', 'build', 'index.html'))
-// })
 
 module.exports = app
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,12 +1,12 @@
 /* @flow */
-import type { $Request, $Response } from 'express'
-import { Paratii } from 'paratii-lib/dist/paratii'
-import { getParatiiConfig } from 'utils/AppUtils'
-
-const fs = require('fs')
-const path = require('path')
-const paratiiConfig = getParatiiConfig(process.env.NODE_ENV)
-const paratii = new Paratii(paratiiConfig)
+// import type { $Request, $Response } from 'express'
+// import { Paratii } from 'paratii-lib/dist/paratii'
+// import { getParatiiConfig } from 'utils/AppUtils'
+//
+// const fs = require('fs')
+// const path = require('path')
+// const paratiiConfig = getParatiiConfig(process.env.NODE_ENV)
+// const paratii = new Paratii(paratiiConfig)
 
 // const path = require('path')
 
@@ -15,55 +15,55 @@ const paratii = new Paratii(paratiiConfig)
 
 // const paratiiConfig = getParatiiConfig(process.env.NODE_ENV)
 // const paratii = new Paratii(paratiiConfig)
-module.exports = async (req: $Request, res: $Response) => {
-  // $FlowFixMe
-  const route = req.route.path
-  let index = fs.readFileSync(
-    path.resolve(__dirname, '../../../', 'build', 'index.html'),
-    'utf8'
-  )
-  let meta = ''
-  let script = ''
-  // script manager
-  switch (route) {
-    case '/embed/:id':
-      script = '<script type="text/javascript" src="/embed/bundle.js"></script>'
-      break
-    default:
-      script = '<script type="text/javascript" src="/bundle.js"></script>'
-      break
-  }
-
-  // route manager
-  switch (route) {
-    case '/embed/:id':
-    case '/play/:id':
-      const { id } = req.params
-      const video = await paratii.core.vids.get(id)
-
-      if (video !== null) {
-        meta = openGraphHead(meta, video)
-        meta = twitterCardHead(meta, video)
-      } else {
-        meta = notFoundVideo(meta)
-      }
-
-      break
-    default:
-      meta = basicHead(meta)
-      script = '<script type="text/javascript" src="/bundle.js"></script>'
-      break
-  }
-
-  index = index.replace('{{meta}}', meta)
-  index = index.replace('{{script}}', script)
-  res.send(index)
-}
-function notFoundVideo (meta) {
+// module.exports = async (req: $Request, res: $Response) => {
+//   // $FlowFixMe
+//   const route = req.route.path
+//   let index = fs.readFileSync(
+//     path.resolve(__dirname, '../../../', 'build', 'index.html'),
+//     'utf8'
+//   )
+//   let meta = ''
+//   let script = ''
+//   // script manager
+//   switch (route) {
+//     case '/embed/:id':
+//       script = '<script type="text/javascript" src="/embed/bundle.js"></script>'
+//       break
+//     default:
+//       script = '<script type="text/javascript" src="/bundle.js"></script>'
+//       break
+//   }
+//
+//   // route manager
+//   switch (route) {
+//     case '/embed/:id':
+//     case '/play/:id':
+//       const { id } = req.params
+//       const video = await paratii.core.vids.get(id)
+//
+//       if (video !== null) {
+//         meta = openGraphHead(meta, video)
+//         meta = twitterCardHead(meta, video)
+//       } else {
+//         meta = notFoundVideo(meta)
+//       }
+//
+//       break
+//     default:
+//       meta = basicHead(meta)
+//       script = '<script type="text/javascript" src="/bundle.js"></script>'
+//       break
+//   }
+//
+//   index = index.replace('{{meta}}', meta)
+//   index = index.replace('{{script}}', script)
+//   res.send(index)
+// }
+exports.notFoundVideo = function notFoundVideo (meta) {
   meta += `<title>Video not found</title>`
   return meta
 }
-function openGraphHead (meta, video) {
+exports.openGraphHead = function openGraphHead (meta, video) {
   const ipfsHash = video.ipfsHash
   const thumbName = video.transcodingStatus.data.result.screenshots[0]
   const thumbnailUrl =
@@ -85,7 +85,7 @@ function openGraphHead (meta, video) {
   return meta
 }
 
-function twitterCardHead (meta, video) {
+exports.twitterCardHead = function twitterCardHead (meta, video) {
   const ipfsHash = video.ipfsHash
   const thumbName = video.transcodingStatus.data.result.screenshots[0]
   const thumbnailUrl =
@@ -108,7 +108,7 @@ function twitterCardHead (meta, video) {
   return meta
 }
 
-function basicHead (meta) {
+exports.basicHead = function basicHead (meta) {
   meta += '<title>Paratii</title>'
   meta += '<meta charset="utf-8">'
   meta +=
