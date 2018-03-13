e65b6fa7c512b287f8c2024e4fa41a26bea1d30f
Elia Pellegrino <elia@piramid.studio>
2018-03-13 19:36:53 -0300

    notification on stake

--- src/scripts/containers/VideoFormContainer.js
+++ src/scripts/containers/VideoFormContainer.js
@@ -4,17 +4,23 @@ import { bindActionCreators } from 'redux'
 import VideoForm from 'components/VideoForm'
 import { openModal } from 'actions/ModalActions'
 import { saveVideoInfo, uploadAndTranscode } from 'actions/UploaderActions'
+import { getUser } from 'selectors/index'
+import { getFormattedPtiBalance } from 'selectors/UserSelectors'
 import { getSelectedUploaderVideo } from 'selectors/UploaderSelectors'
 import type { RootState } from 'types/ApplicationTypes'
+import { show } from 'react-notification-system-redux'
 
 const mapStateToProps = (state: RootState) => ({
-  selectedVideo: getSelectedUploaderVideo(state)
+  selectedVideo: getSelectedUploaderVideo(state),
+  user: getUser(state),
+  balance: getFormattedPtiBalance(state)
 })
 
 const mapDispatchToProps = dispatch => ({
   saveVideoInfo: bindActionCreators(saveVideoInfo, dispatch),
   openModal: bindActionCreators(openModal, dispatch),
-  uploadAndTranscode: bindActionCreators(uploadAndTranscode, dispatch)
+  uploadAndTranscode: bindActionCreators(uploadAndTranscode, dispatch),
+  notification: bindActionCreators(show, dispatch)
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(VideoForm)
