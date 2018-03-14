35bd6e0bc5eb55365dd6509593f66b33bdf2c5f4
Benjamin Baum <bjb253@cornell.edu>
2018-03-13 23:35:01 -0400

    47 - revert popover changes

--- src/scripts/components/foundations/Popover.js
+++ src/scripts/components/foundations/Popover.js
@@ -5,26 +5,22 @@ import styled from 'styled-components'
 
 type Props = {
   children: any,
-  open: boolean,
   style?: Object
 }
 
 const Wrapper = styled.div`
   width: 100%;
-  height: ${({ open }) => (open ? '100%' : '0px')};
+  height: 100%;
   border: ${({ theme }) => `1px solid ${theme.colors.popover.border}`};
   border-radius: 4px;
   background-color: ${({ theme }) => theme.colors.popover.background};
   padding: 20px;
-  opacity: ${({ open }) => (open ? 1.0 : 0)};
-  transition: all 250ms ${({ theme }) => theme.animation.ease.smooth};
 `
 
 class Popover extends React.Component<Props, void> {
   render () {
-    const { open } = this.props
     return (
-      <Wrapper onClick={(e: Object) => e.stopPropagation()} open={open}>
+      <Wrapper onClick={(e: Object) => e.stopPropagation()}>
         {this.props.children}
       </Wrapper>
     )
