a00d4c9c803f64f3bd7bca7405f1eaf996395dfe
Benjamin Baum <bjb253@cornell.edu>
2018-04-08 01:03:38 -0400

    342 - correctly configure safari10 fix

--- webpack.config.js
+++ webpack.config.js
@@ -135,14 +135,14 @@ const config = {
             reserved:
             ['DAGNode', 'DAGLink','Name', 'Tsize', 'Hash', 'Block', '_idB58String',
             'Multiaddr', 'WebSockets'],
-            safari10: true
             // NOTE I'm still working on this. keep this commented out for now.
             // ['_links', 'links', '_data', 'data', 'mutlihash', '_multihash',
             //   'serialized', '_serialized', 'size', '_size',
             // '_id', 'peerId._id', 'peerId._idB58String', 'peerId', 'id', 'multiaddr',
             // 'multiaddrs', 'buffer', 'Buffer',
           },
-          compress: false
+          compress: false,
+          safari10: true
         }
       })
     : new webpack.HotModuleReplacementPlugin(),
