1e673c48a4892683ca448d350fe4057ca12f09c5
Yahya <ya7yaz@gmail.com>
2018-04-09 15:22:44 +0200

    lib to js

--- src/scripts/setupDevEnv.js
+++ src/scripts/setupDevEnv.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const { Paratii } = require('paratii-lib')
+const { Paratii } = require('paratii-js')
 
 const env = process.env.NODE_ENV || 'development'
 
