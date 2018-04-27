e89f62320675c5a7f6d2a98c21cc1e851ab20d64
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-04-27 16:24:35 +0200

    ...

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,14 @@
 
 
 if [[ $1 == staging ]]; then
-    # ok
     echo "deploying to $1"
-    rm -r build
     yarn run build:staging
-    rsync -avzh paratii-portal paratii@staging.paratii.video:/home/paratii/
+    rsync -e 'ssh -o StrictHostKeyChecking=no'  -avzh . paratii@staging.paratii.video:/home/paratii/
+    ssh -o StrictHostKeyChecking=no paratii@staging.paratii.video << EOF
+      sh restart.sh
+EOF
     # ssh -o StrictHostKeyChecking=no paratii@build.paratii.video "bash ./devops/deploy_portal.sh staging $1 </dev/null"
 elif [[ $1 == production ]]; then
-    # ok
     echo "deploying to $1"
     ssh -o StrictHostKeyChecking=no paratii@build.paratii.video "bash ./devops/deploy_portal.sh production $1 </dev/null"
 else
