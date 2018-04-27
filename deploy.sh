78b2ccbf963f64f437132715c8db7d903e44ffa4
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-04-27 15:38:55 +0200

    trying to bypass the build server

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,10 @@
 if [[ $1 == staging ]]; then
     # ok
     echo "deploying to $1"
-    ssh -o StrictHostKeyChecking=no paratii@build.paratii.video "bash ./devops/deploy_portal.sh staging $1 </dev/null"
+    rm -r build
+    yarn run build:staging
+    rsync -avzh paratii-portal paratii@staging.paratii.video:/home/paratii/
+    # ssh -o StrictHostKeyChecking=no paratii@build.paratii.video "bash ./devops/deploy_portal.sh staging $1 </dev/null"
 elif [[ $1 == production ]]; then
     # ok
     echo "deploying to $1"
