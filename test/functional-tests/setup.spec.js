2add2cff94e86e2d29c8d6fd53580bfdd1bbdbd6
Benjamin Baum <bjb253@cornell.edu>
2018-04-27 20:45:40 -0400

    515 - fix test

--- test/functional-tests/setup.spec.js
+++ test/functional-tests/setup.spec.js
@@ -133,10 +133,3 @@ before(async function (done) {
   //   browser.contracts = await getOrDeployParatiiContracts(server, browser)
   done()
 })
-
-beforeEach(() => {
-  browser.setViewportSize({
-    width: 2000,
-    height: 2000
-  })
-})
