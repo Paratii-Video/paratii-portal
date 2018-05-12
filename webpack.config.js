5e170c2a8488da9be6dc5b2b1f7a2085a2b8ca16
Benjamin Baum <bjb253@cornell.edu>
2018-05-12 18:56:27 -0400

    Import minified clappr

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,7 @@ const config = {
   target: 'web',
   resolve: {
     alias: {
+      clappr: 'clappr/dist/clappr.min',
       config: configDir,
       scripts: scriptsDir,
       styles: stylesDir,
