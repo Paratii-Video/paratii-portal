96c7a8944f346473f9d48fdf9b095f447d4c97c8
jrgarou <jrgarou@gmail.com>
2018-05-22 20:07:31 -0300

    ButtonColor + ButtonStyle

--- src/scripts/components/foundations/Button.js
+++ src/scripts/components/foundations/Button.js
@@ -1,5 +1,27 @@
 import styled, { css } from 'styled-components'
 
+export const ButtonStyleBackground = css`
+  ${props => {
+    let _background: String
+
+    if (props.white) {
+      _background = props.theme.colors.button.white
+    } else if (props.gray) {
+      _background = props.theme.colors.button.gray
+    } else if (props.pink) {
+      _background = props.theme.colors.button.pink
+    } else if (props.green) {
+      _background = props.theme.colors.button.green
+    } else {
+      _background = props.theme.colors.button.purple
+    }
+
+    return css`
+      background: ${_background};
+`
+  }};
+`
+
 export const ButtonStyleColor = css`
   ${props => {
     let _color: String
@@ -10,6 +32,8 @@ export const ButtonStyleColor = css`
       _color = props.theme.colors.button.purple
     } else if (props.pink) {
       _color = props.theme.colors.button.pink
+    } else if (props.green) {
+      _color = props.theme.colors.button.green
     } else {
       _color = props.theme.colors.button.gray
     }
@@ -40,6 +64,20 @@ export const StyleAnchor = css`
   }};
 `
 
+export const ButtonStyle = css`
+  cursor: ${props => (props.disabled ? 'initial' : 'pointer')};
+  font-size: ${props => (props.anchor ? null : props.theme.fonts.button)};
+  font-weight: ${props =>
+    props.anchor
+      ? props.theme.fonts.weight.regular
+      : props.theme.fonts.weight.bold};
+  margin: ${props => props.margin};
+  opacity: ${props => (props.disabled ? 0.5 : null)};
+  pointer-events: ${props => (props.disabled ? 'none' : null)};
+  text-transform: ${props => (props.anchor ? null : 'uppercase')};
+  user-select: ${props => (props.anchor ? null : 'none')};
+`
+
 export const ButtonStyleHover = css`
   backface-visibility: hidden;
   transition: opacity ${props => props.theme.animation.time.repaint};
@@ -49,19 +87,14 @@ export const ButtonStyleHover = css`
   }
 `
 
+export const ButtonColor = styled.button`
+  ${ButtonStyle} ${ButtonStyleHover} ${ButtonStyleBackground} border-radius: 2px;
+  color: ${props => props.theme.colors.button.white};
+  padding: 12px;
+`
+
 const Button = styled.button`
-  ${StyleAnchor} ${ButtonStyleColor} ${ButtonStyleHover} cursor: ${props =>
-  props.disabled ? 'initial' : 'pointer'};
-  font-size: ${props => (props.anchor ? null : props.theme.fonts.button)};
-  font-weight: ${props =>
-    props.anchor
-      ? props.theme.fonts.weight.regular
-      : props.theme.fonts.weight.bold};
-  margin: ${props => props.margin};
-  opacity: ${props => (props.disabled ? 0.5 : 1)};
-  pointer-events: ${props => (props.disabled ? 'none' : null)};
-  text-transform: ${props => (props.anchor ? null : 'uppercase')};
-  user-select: ${props => (props.anchor ? null : 'none')};
+  ${StyleAnchor} ${ButtonStyleColor} ${ButtonStyle} ${ButtonStyleHover};
 `
 
 export default Button
