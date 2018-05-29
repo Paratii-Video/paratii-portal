3d3e8a4cf12c38a0aa497127d8f59fb53da86550
jrgarou <jrgarou@gmail.com>
2018-05-29 16:59:24 -0300

    remove ProfileHeader

--- src/scripts/components/ProfileMyVideos.js
+++ src/scripts/components/ProfileMyVideos.js
@@ -10,8 +10,6 @@ type Props = {
   videos: Map<string, VideoRecord> // maps video ids to upload records
 }
 
-const PROFILEPICTURE_SIZE = '112px'
-
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -20,53 +18,11 @@ const Wrapper = styled.div`
   width: 100%;
 `
 
-const ProfileHeader = styled.div`
-  background: ${props =>
-    props.theme.colors.ProfileMyVideos.headerImageBackground};
-  display: flex;
-  flex-direction: column;
-`
-const ProfileHeaderCover = styled.div`
-  background: url(${({ source }) => source}) no-repeat 50%;
-  background-size: cover;
-  height: 210px;
-`
-
-const ProfileHeaderInfo = styled.div`
-  align-items: center;
-  background: ${props =>
-    props.theme.colors.ProfileMyVideos.headerInfoBackground};
-  display: flex;
-  flex-direction: column;
-  padding: 0 42px 24px;
-`
-
-const ProfileHeaderPicture = styled.div`
-  background: ${props =>
-    props.theme.colors.ProfileMyVideos.ProfilePictureBackground}
-    url(${({ source }) => source}) no-repeat 50%;
-  background-size: cover;
-  border: 8px solid
-    ${props => props.theme.colors.ProfileMyVideos.ProfilePictureBackground};
-  border-radius: 100%;
-  height: ${PROFILEPICTURE_SIZE};
-  transform: translate3d(0, -50%, 0);
-  width: ${PROFILEPICTURE_SIZE};
-`
-
-const ProfileHeaderInfoWrapper = styled.div`
-  align-items: center;
-  display: flex;
-  justify-content: space-around;
-  width: 100%;
-`
-
 const ProfileFilterVideos = styled.div`
   align-items: center;
   background: ${props => props.theme.colors.ProfileMyVideos.filterBackground};
   display: flex;
   justify-content: space-between;
-  margin-top: 48px;
   padding: 30px 48px;
 `
 
@@ -87,15 +43,6 @@ class ProfileMyVideos extends Component<Props, void> {
   render () {
     return (
       <Wrapper>
-        <ProfileHeader>
-          <ProfileHeaderCover source="http://www.redbullsignatureseries.com/wp-content/uploads/2017/03/P-20160515-00063_News-720x405.jpg" />
-          <ProfileHeaderInfo>
-            <ProfileHeaderPicture source="http://www.redbullsignatureseries.com/wp-content/uploads/2017/03/P-20160515-00063_News-720x405.jpg" />
-            <ProfileHeaderInfoWrapper>
-              <Title bold>User 12610549</Title>
-            </ProfileHeaderInfoWrapper>
-          </ProfileHeaderInfo>
-        </ProfileHeader>
         <ProfileFilterVideos>
           <ProfileFilterTitle small>My videos</ProfileFilterTitle>
           <ProfileFilterRadioWrapper>
