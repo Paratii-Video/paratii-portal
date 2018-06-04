0fd33463b04d3ad65ce1b2c1f5ce42cc6eaded97
Benjamin Baum <bjb253@cornell.edu>
2018-06-03 20:20:09 -0400

    668 - remove @watch

--- test/functional-tests/wallet.test.js
+++ test/functional-tests/wallet.test.js
@@ -13,7 +13,7 @@ import {
 } from './test-utils/helpers.js'
 import { assert } from 'chai'
 
-describe('💰 Wallet: @watch', function () {
+describe('💰 Wallet:', function () {
   let userAccount
 
   beforeEach(function () {
