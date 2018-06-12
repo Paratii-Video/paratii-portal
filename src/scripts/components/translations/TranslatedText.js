5167d1a35f658dbd823cfc9ea0425d9eb276dde1
Benjamin Baum <bjb253@cornell.edu>
2018-06-11 21:32:14 -0400

    676 - support rendering jsx in translations

--- src/scripts/components/translations/TranslatedText.js
+++ src/scripts/components/translations/TranslatedText.js
@@ -2,13 +2,15 @@
 
 import React from 'react'
 
+import { HTML_KEY_REGEX } from 'constants/TranslationsConstants'
 import ConnectToTranslator from 'utils/translations/ConnectToTranslator'
+import renderRawTranslatable from 'utils/translations/renderRawTranslatable'
 
 import type { Translator } from 'types/TranslationTypes'
 
 type Props = {
   message: string,
-  options?: ?Object,
+  options?: Object,
   translator?: Translator
 }
 
@@ -20,8 +22,33 @@ class TranslatedText extends React.Component<Props> {
       return ''
     }
 
+    if (HTML_KEY_REGEX.test(message)) {
+      // $FlowFixMe
+      const processOptions: Object = !options
+        ? {}
+        : Object.keys(options).reduce(
+          (reducedOptions: Object, optionsKey: string): Object => {
+            reducedOptions[optionsKey] = renderRawTranslatable(
+              options[optionsKey]
+            )
+            return reducedOptions
+          },
+          {}
+        )
+
+      const translatedString: string = translator({
+        key: message,
+        options: processOptions
+      })
+      return <span dangerouslySetInnerHTML={{ __html: translatedString }} />
+    }
+
     return translator({ key: message, options })
   }
+}
+
+TranslatedText.defaultProps = {
+  options: {}
 }
 
 export default ConnectToTranslator(TranslatedText)
