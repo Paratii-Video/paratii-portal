fafa8ceb1c0b5843ad51b38e07a8c50d05dea838
Benjamin Baum <bjb253@cornell.edu>
2018-06-14 16:45:42 -0500

    684 - rename export

--- src/scripts/components/tipping/utils/TippingStepHeader.js
+++ src/scripts/components/tipping/utils/TippingStepHeader.js
@@ -4,10 +4,10 @@ import styled from 'styled-components'
 
 import Colors from 'components/foundations/base/Colors'
 
-const Header = styled.div`
+const TippingStepHeader = styled.div`
   color: ${Colors.white};
   font-weight: bold;
   font-size: 25px;
 `
 
-export default Header
+export default TippingStepHeader
