6fd25f22a55853e3be6e10b9bf275b51c65e378f
Benjamin Baum <bjb253@cornell.edu>
2018-06-15 11:22:57 -0500

    684 - more enter password UX

--- src/scripts/components/tipping/steps/EnterPasswordTipStep.js
+++ src/scripts/components/tipping/steps/EnterPasswordTipStep.js
@@ -3,8 +3,11 @@
 import React, { Fragment } from 'react'
 import styled from 'styled-components'
 
+import paratii, { getSecureWallet } from 'utils/ParatiiLib'
+
 import RawTranslatedText from 'utils/translations/RawTranslatedText'
 
+import Colors from 'components/foundations/base/Colors'
 import Button from 'components/foundations/Button'
 import Input from 'components/foundations/forms/Input'
 import TranslatedText from 'components/translations/TranslatedText'
@@ -12,7 +15,9 @@ import TranslatedText from 'components/translations/TranslatedText'
 import TippingStepHeader from '../utils/TippingStepHeader'
 import TipAmount from '../utils/TipAmount'
 
-const PasswordForm = styled.div`
+const FORM_ID: string = 'TIP_PASSWORD_FORM'
+
+const PasswordForm = styled.form`
   display: flex;
   flex-direction: column;
   width: 65%;
@@ -23,9 +28,24 @@ const TipAmountWrapper = styled.div`
   margin-top: 10px;
 `
 
+const BottomBar = styled.div`
+  display: flex;
+  width: 100%;
+  height: 20px;
+  margin-top: 25px;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+const PasswordError = styled.div`
+  color: ${Colors.pink};
+  flex: 0 1 100%;
+  text-align: left;
+`
+
 const ContinueButton = styled(Button)`
-  margin-top: 20px;
-  align-self: flex-end;
+  align-self: baseline;
+  justify-self: flex-end;
 `
 
 type Props = {
@@ -54,6 +74,24 @@ class EnterPasswordTipStep extends React.Component<Props, State> {
     })
   }
 
+  onSubmitPassword = (e: Object): void => {
+    e.preventDefault()
+
+    const walletString: string = getSecureWallet()
+
+    try {
+      paratii.eth.wallet.decrypt(JSON.parse(walletString), this.state.password)
+      this.setState({
+        showPasswordError: false
+      })
+      this.props.onSuccessfulAuth()
+    } catch (e) {
+      this.setState({
+        showPasswordError: true
+      })
+    }
+  }
+
   render () {
     return (
       <Fragment>
@@ -63,17 +101,30 @@ class EnterPasswordTipStep extends React.Component<Props, State> {
         <TipAmountWrapper>
           <TipAmount amount={this.props.tipAmount} />
         </TipAmountWrapper>
-        <PasswordForm>
+        <PasswordForm id={FORM_ID} onSubmit={this.onSubmitPassword}>
           <Input
+            type="password"
+            error={this.state.showPasswordError}
             placeholder={RawTranslatedText({
               message: 'tipping.steps.enterPassword.inputPlaceholder'
             })}
             onChange={this.onPasswordChange}
             value={this.state.password}
           />
-          <ContinueButton disabled={!this.state.password.trim()} type="submit">
-            Continue
-          </ContinueButton>
+          <BottomBar>
+            {this.state.showPasswordError && (
+              <PasswordError>
+                <TranslatedText message="tipping.steps.enterPassword.error" />
+              </PasswordError>
+            )}
+            <ContinueButton
+              form={FORM_ID}
+              disabled={!this.state.password.trim()}
+              type="submit"
+            >
+              <TranslatedText message="tipping.steps.enterPassword.continue" />
+            </ContinueButton>
+          </BottomBar>
         </PasswordForm>
       </Fragment>
     )
--- src/scripts/utils/ParatiiLib.js
+++ src/scripts/utils/ParatiiLib.js
@@ -9,9 +9,9 @@ import type { ParatiiLib } from 'types/ApplicationTypes'
 const paratiiConfig = getParatiiConfig(process.env.NODE_ENV, 'client')
 window.paratii = new Paratii(paratiiConfig)
 
-const paratii: ParatiiLib = window.paratii
-
 export const getSecureWallet = (): string =>
   localStorage.getItem(WALLET_KEY_SECURE) || ''
+
+const paratii: ParatiiLib = window.paratii
 
 export default paratii
