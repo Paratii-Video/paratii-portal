a230cba2180124d156b3dc041ccd37c55f10535c
Benjamin Baum <bjb253@cornell.edu>
2018-06-17 15:37:36 -0500

    684 - do not ask for tip if wallet not secure

--- src/scripts/selectors/TippingSelectors.js
+++ src/scripts/selectors/TippingSelectors.js
@@ -8,16 +8,24 @@ import {
   getDoNotTipVideoIds
 } from 'selectors/index'
 import { getPlayingVideo } from 'selectors/PlayerSelectors'
+import { getIsSecure } from 'selectors/UserSelectors'
 
 import Video from 'records/VideoRecords'
 
 import type { RootState } from 'types/ApplicationTypes'
 
 export const askForTip: (state: RootState) => boolean = createSelector(
-  [getPlayerCurrentTimeSeconds, getDoNotTipVideoIds, getPlayingVideo],
+  [
+    getIsSecure,
+    getPlayerCurrentTimeSeconds,
+    getDoNotTipVideoIds,
+    getPlayingVideo
+  ],
   (
+    isSecure: boolean,
     currentTimeSeconds: number,
     doNotTipVideoIds: Map<string, boolean>,
     video: Video
-  ) => currentTimeSeconds > 3 && !doNotTipVideoIds.get(video.get('id'))
+  ) =>
+    isSecure && currentTimeSeconds > 3 && !doNotTipVideoIds.get(video.get('id'))
 )
