adb5ff80bf0948f6ddf8ef6721fd14e77beba4d0
Benjamin Baum <bjb253@cornell.edu>
2018-06-18 14:39:36 -0500

    684 - show tip button even if account not secured

--- src/scripts/selectors/TippingSelectors.js
+++ src/scripts/selectors/TippingSelectors.js
@@ -8,26 +8,16 @@ import {
   getPlayerTotalTimeViewedSeconds
 } from 'selectors/index'
 import { getPlayingVideo } from 'selectors/PlayerSelectors'
-import { getIsSecure } from 'selectors/UserSelectors'
 
 import Video from 'records/VideoRecords'
 
 import type { RootState } from 'types/ApplicationTypes'
 
 export const askForTip: (state: RootState) => boolean = createSelector(
-  [
-    getIsSecure,
-    getPlayerTotalTimeViewedSeconds,
-    getDoNotTipVideoIds,
-    getPlayingVideo
-  ],
+  [getPlayerTotalTimeViewedSeconds, getDoNotTipVideoIds, getPlayingVideo],
   (
-    isSecure: boolean,
     totalTimeViewedSeconds: number,
     doNotTipVideoIds: Map<string, boolean>,
     video: Video
-  ) =>
-    isSecure &&
-    totalTimeViewedSeconds > 3 &&
-    !doNotTipVideoIds.get(video.get('id'))
+  ) => totalTimeViewedSeconds > 3 && !doNotTipVideoIds.get(video.get('id'))
 )
