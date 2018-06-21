657e15ff449365efd5b3031196816005008341d8
Benjamin Baum <bjb253@cornell.edu>
2018-06-21 19:10:44 -0400

    684 - do not show tipping button if you lack enough pti for minimum tip amount

--- src/scripts/selectors/TippingSelectors.js
+++ src/scripts/selectors/TippingSelectors.js
@@ -8,20 +8,45 @@ import {
   getPlayerTotalTimeViewedSeconds
 } from 'selectors/index'
 import { getPlayingVideo } from 'selectors/PlayerSelectors'
+import { getFormattedPtiBalance } from 'selectors/UserSelectors'
 
 import Video from 'records/VideoRecords'
 
-import { MINIMUM_VIEWED_SECONDS_FOR_TIP_PROMPT } from 'constants/TippingConstants'
+import {
+  MINIMUM_VIEWED_SECONDS_FOR_TIP_PROMPT,
+  TIPPING_PTI_AMOUNTS
+} from 'constants/TippingConstants'
 
 import type { RootState } from 'types/ApplicationTypes'
 
+const getTippingAmounts = (): Array<number> => TIPPING_PTI_AMOUNTS
+
+const getSortedTippingAmounts: (
+  state: RootState
+) => Array<number> = createSelector(
+  [getTippingAmounts],
+  (amounts: Array<number>): Array<number> => amounts.sort()
+)
+
+const userHasEnoughPtiToTip: (state: RootState) => boolean = createSelector(
+  [getSortedTippingAmounts, getFormattedPtiBalance],
+  (amounts: Array<number>, ptiBalance: string) => `${amounts[0]}` <= ptiBalance
+)
+
 export const askForTip: (state: RootState) => boolean = createSelector(
-  [getPlayerTotalTimeViewedSeconds, getDoNotTipVideoIds, getPlayingVideo],
+  [
+    getPlayerTotalTimeViewedSeconds,
+    getDoNotTipVideoIds,
+    getPlayingVideo,
+    userHasEnoughPtiToTip
+  ],
   (
     totalTimeViewedSeconds: number,
     doNotTipVideoIds: Map<string, boolean>,
-    video: Video
+    video: Video,
+    userHasEnoughPtiToTip: boolean
   ) =>
+    userHasEnoughPtiToTip &&
     totalTimeViewedSeconds > MINIMUM_VIEWED_SECONDS_FOR_TIP_PROMPT &&
     !doNotTipVideoIds.get(video.get('id'))
 )
