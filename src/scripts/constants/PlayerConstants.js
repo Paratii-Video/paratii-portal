03e2a233522a9aae4fa217e9edb5ec2d5dda56cb
Benjamin Baum <bjb253@cornell.edu>
2018-07-09 21:05:06 -0400

    766 - remove wallet from video player

--- src/scripts/constants/PlayerConstants.js
+++ src/scripts/constants/PlayerConstants.js
@@ -3,8 +3,7 @@
 const makePluginConstant = (plugin: string): string => `PLAYER_PLUGIN_${plugin}`
 
 export const PLAYER_PLUGIN: Object = {
-  PLAYBACK_LEVELS: makePluginConstant('PLAYBACK_LEVEL'),
-  WALLET: makePluginConstant('WALLET')
+  PLAYBACK_LEVELS: makePluginConstant('PLAYBACK_LEVEL')
 }
 
 export const PLAYER_PARAMS: Object = {
