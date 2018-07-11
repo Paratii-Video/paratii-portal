3233d79ff39e39507f58e0d681d1f03d8f5e753d
Elia Pellegrino <elia@piramid.studio>
2018-07-11 15:50:15 +0200

    using placeholder

--- src/scripts/actions/TCRActions.js
+++ src/scripts/actions/TCRActions.js
@@ -2,27 +2,11 @@
 
 import { createAction } from 'redux-actions'
 import type { Dispatch } from 'redux'
-import {
-  VOTE_VIDEO,
-  VOTE_STATUS,
-  UPDATE_CHALLENGE
-} from 'constants/ActionConstants'
+import { VOTE_VIDEO, VOTE_STATUS } from 'constants/ActionConstants'
 import { VOTE_STATE } from 'constants/TCRConstants'
 
 export const voteVideo = createAction(VOTE_VIDEO)
 export const voteStatus = createAction(VOTE_STATUS)
-export const fetchChallengeSuccess = createAction(UPDATE_CHALLENGE)
-
-// export const fetchVoteStatus = (videoId: string) => async (
-//   dispatch: Dispatch<*>
-// ) => {
-//   // FIXME get voteStatus from DB
-//   const voteStatusValue = VOTE_STATE.COMMITTED
-//   console.log(voteStatusValue)
-//   if (voteStatusValue) {
-//     dispatch(voteStatus({ id: videoId, voteStatus: voteStatusValue }))
-//   }
-// }
 
 export const fetchChallenge = (videoId: string) => async (
   dispatch: Dispatch<*>
@@ -30,30 +14,56 @@ export const fetchChallenge = (videoId: string) => async (
   // FIXME get challenge from DB
   try {
     // const challenge = await paratii.eth.tcr.challenge.get(videoId)
-    const voteStatusValue = VOTE_STATE.COMMITTED
-    console.log(voteStatusValue)
-    if (voteStatusValue) {
-      dispatch(
-        fetchChallengeSuccess({
-          id: videoId,
-          voteStatus: voteStatusValue
-        })
-      )
+    // Fixture
+    const challenge = {
+      id: 0x1234,
+      listingHash: 0x12345,
+      rewardPool: 5e18,
+      challenger: 0x12345,
+      resolved: true,
+      stake: 10e18,
+      totalTokens: 14e18,
+      voterCanClaimReward: false,
+      commitStartDate: 1530741082,
+      commitEndDate: 1530741182,
+      // revealStartDate (aka commitEndDate)
+      revealEndDate: 1530741282,
+      voteQuorum: 30,
+      votesFor: 3e18,
+      votesAgainst: 4e18
     }
-  } catch (e) {}
-}
+    console.log(challenge)
 
-export const fetchUserVote = (videoId: string) => async (
-  dispatch: Dispatch<*>
-) => {
-  try {
-    // FIXME get user vote from DB
-    // get vote value
-    const voteValue = 1
-    dispatch(voteVideo({ id: videoId, vote: voteValue }))
-    // get vote status
-    const voteStatusValue = VOTE_STATE.COMMITTED
+    // const userVote = await paratii.eth.tcr.votes.get(challenge.id, paratii.getAccount())
+    const userVote = {
+      total: 1,
+      hasNext: false,
+      results: [
+        {
+          id: 12354,
+          voter: 0x12454,
+          pollID: 0x12354,
+          numTokens: 2e18,
+          choice: 0, // choice is either 0 or 1, or null
+          voteCommitted: 1245, // timestamp,
+          voteRevealed: 12345
+        }
+      ]
+    }
+
+    const userVoteResult = userVote.results[0]
+    let voteStatusValue = ''
+    if (userVote && userVoteResult.voteCommitted) {
+      voteStatusValue = VOTE_STATE.COMMITTED
+    } else if (userVote && userVoteResult.voteSent) {
+      voteStatusValue = VOTE_STATE.SENT
+    } else if (userVote && userVoteResult.voteRevealed) {
+      voteStatusValue = VOTE_STATE.REVEALED
+    }
+
+    dispatch(voteVideo({ id: videoId, vote: userVoteResult.choice }))
     dispatch(voteStatus({ id: videoId, voteStatus: voteStatusValue }))
-    // get total token staked for this video
-  } catch (e) {}
+  } catch (e) {
+    console.log(e.message)
+  }
 }
