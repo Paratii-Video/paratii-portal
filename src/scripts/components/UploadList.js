1c3e54090c69d2f16842ae76b02359adc530c0df
jrgarou <jrgarou@gmail.com>
2018-07-17 16:10:00 -0300

    At /upload only show video !== isVideoPublishable in UploadList

--- src/scripts/components/UploadList.js
+++ src/scripts/components/UploadList.js
@@ -5,6 +5,7 @@ import UploadListItem from 'containers/UploadListItemContainer'
 import Loader from 'components/foundations/Loader'
 import type { Map } from 'immutable'
 import type VideoRecord from 'records/VideoRecords'
+import { isVideoPublishable } from 'operators/VideoOperators'
 
 type Props = {
   videosAreBeingFetched: boolean,
@@ -15,7 +16,6 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1 0 100%;
-  min-height: 200px;
 `
 
 class UploadList extends Component<Props, void> {
@@ -25,15 +25,17 @@ class UploadList extends Component<Props, void> {
         {this.props.videosAreBeingFetched ? (
           <Loader />
         ) : (
-          this.props.videos
-            .entrySeq()
-            .map(([videoId, videoInfo]) => (
-              <UploadListItem
-                key={videoId}
-                videoId={videoId}
-                video={videoInfo}
-              />
-            ))
+          this.props.videos.entrySeq().map(([videoId, videoInfo]) => {
+            if (!isVideoPublishable(videoInfo)) {
+              return (
+                <UploadListItem
+                  key={videoId}
+                  videoId={videoId}
+                  video={videoInfo}
+                />
+              )
+            }
+          })
         )}
       </Wrapper>
     )
