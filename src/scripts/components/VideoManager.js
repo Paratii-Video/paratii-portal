2d0ea10709fbf45b64c0daa6f4fcb5d02ea93363
jrgarou <jrgarou@gmail.com>
2018-07-18 00:36:18 -0300

    fixing FileUploaderWrapper display

--- src/scripts/components/VideoManager.js
+++ src/scripts/components/VideoManager.js
@@ -39,6 +39,8 @@ const Wrapper = styled.div`
 `
 
 const FileUploaderWrapper = styled.div`
+  display: flex;
+  flex: 1 1 100%;
   margin: ${({ margin }) =>
     margin ? VIDEOMANAGER_FILEUPLOADERWRAPPER_MARGIN : null};
 `
