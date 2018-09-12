ff06303696a470bd53c1586ca5fdaa1683077c3d
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2018-09-12 09:36:58 +0200

    add some more vids

--- src/scripts/constants/CategoriesConstants.js
+++ src/scripts/constants/CategoriesConstants.js
@@ -126,12 +126,6 @@ export const VIDEO_CATEGORIES: Array<Object> = [
         time: '00:38'
       },
       {
-        title: 'A Conversation with Naval Ravikant and Ryan Shea | Blockstack Summit 2017 ',
-        url: '/play/p1TyceTSWr8p',
-        image: 'https://gateway.paratii.video/ipfs/QmRutqHVzdyDFc2UZT3nmu4aeqdBCwG2fxmqiXBz5GQjPx/thumbnail-1280x720_1.png',
-        time: '31:02'
-      },
-      {
         title: 'ETH Buenos Aires | The ETHCommunity Speaks: Chris Burniske ',
         url: '/play/On1UzFx05tk4',
         image: 'https://gateway.paratii.video/ipfs/QmZL2Pe4K62GjtzHgDgHdJG3eTRigdgxzcLqQBWVLiQEqc/thumbnail-1280x720_1.png',
@@ -144,52 +138,10 @@ export const VIDEO_CATEGORIES: Array<Object> = [
         time: '03:09'
       },
       {
-        title: 'Around the Block - Trailer',
-        url: '/play/a4Dbd26pLu0X',
-        image: '/assets/img/landing/small/around-the-block-trailer.png',
-        time: '02:56'
-      },
-      {
-        title: 'Todo Tempo do Mundo - Ep. 1',
-        url: '/play/mF7YHwBeGqZq',
-        image: '/assets/img/landing/small/todo-tempo-do-mundo-ep-1.png',
-        time: '18:38'
-      },
-      {
-        title: 'O que é Algoritmo',
-        url: '/play/D93ftQHK3OXN',
-        image: '/assets/img/landing/small/o-que-e-algoritimo.png',
-        time: '02:48'
-      },
-      {
-        title: 'Só Quero Fazer Falta',
-        url: '/play/9Jh8KlGxtRIC',
-        image: '/assets/img/landing/small/so-quero-fazer-falta.png',
-        time: '02:03'
-      },
-      {
-        title: 'Blockstack Berlin: The Decentralized Future',
-        url: '/play/Jggpz1FBZKRW',
-        image: 'https://gateway.paratii.video/ipfs/QmVVx3bGz9sqGNqkziFd7qAccGPdE1DrCzd9H8batsPMq5/thumbnail-1280x720_1.png',
-        time: '00:38'
-      },
-      {
         title: 'A Conversation with Naval Ravikant and Ryan Shea | Blockstack Summit 2017 ',
         url: '/play/p1TyceTSWr8p',
         image: 'https://gateway.paratii.video/ipfs/QmRutqHVzdyDFc2UZT3nmu4aeqdBCwG2fxmqiXBz5GQjPx/thumbnail-1280x720_1.png',
         time: '31:02'
-      },
-      {
-        title: 'ETH Buenos Aires | The ETHCommunity Speaks: Chris Burniske ',
-        url: '/play/On1UzFx05tk4',
-        image: 'https://gateway.paratii.video/ipfs/QmZL2Pe4K62GjtzHgDgHdJG3eTRigdgxzcLqQBWVLiQEqc/thumbnail-1280x720_1.png',
-        time: '02:15'
-      },
-      {
-        title: 'ETHBuenosAires 2018: The final cut',
-        url: '/play/yFnPGUOvLZm8',
-        image: 'https://gateway.paratii.video/ipfs/QmYvaVc8qaFegRNZRzQ1r2yDm1sNpjPhP1d9VpvPoZ3FKY/thumbnail-1280x720_1.png',
-        time: '03:09'
       }
     ]
   },
@@ -224,28 +176,16 @@ export const VIDEO_CATEGORIES: Array<Object> = [
         time: '02:03'
       },
       {
-        title: 'O que é Algoritmo',
-        url: '/play/D93ftQHK3OXN',
-        image: '/assets/img/landing/small/o-que-e-algoritimo.png',
-        time: '02:48'
+        title: 'Introducing Filecoin, a decentralized storage network',
+        url: '/play/JGtkGxoca0c9',
+        image: 'https://gateway.paratii.video/ipfs/QmYeELev8HiXy9mUnFpV4QxK2u9htKvqCLvgySexkUFmNT/thumbnail-1280x554_1.png',
+        time: '05:29'
       },
       {
-        title: 'Around the Block - Trailer',
-        url: '/play/a4Dbd26pLu0X',
-        image: '/assets/img/landing/small/around-the-block-trailer.png',
-        time: '02:56'
-      },
-      {
-        title: 'Todo Tempo do Mundo - Ep. 1',
-        url: '/play/mF7YHwBeGqZq',
-        image: '/assets/img/landing/small/todo-tempo-do-mundo-ep-1.png',
-        time: '18:38'
-      },
-      {
-        title: 'Só Quero Fazer Falta',
-        url: '/play/9Jh8KlGxtRIC',
-        image: '/assets/img/landing/small/so-quero-fazer-falta.png',
-        time: '02:03'
+        title: 'The Golem Project',
+        url: '/play/UoljEF8DRyMC',
+        image: 'https://gateway.paratii.video/ipfs/QmScUMD1bQzST7Rcb8eDasyMtV46KF6SfGLhEuHMuYjJJu/thumbnail-1280x720_1.png',
+        time: '06.33'
       }
     ]
   },
--- src/scripts/constants/LandingConstants.js
+++ src/scripts/constants/LandingConstants.js
@@ -85,28 +85,28 @@ export const LANDING_CATEGORY_VIDEOS: Array<Object> = [
     slug: '/category/community-events',
     list: [
       {
-        title: 'O que é Algoritmo',
-        url: '/play/D93ftQHK3OXN',
-        image: '/assets/img/landing/small/o-que-e-algoritimo.png',
-        time: '02:48'
+        title: 'Blockstack Berlin: The Decentralized Future',
+        url: '/play/Jggpz1FBZKRW',
+        image: 'https://gateway.paratii.video/ipfs/QmVVx3bGz9sqGNqkziFd7qAccGPdE1DrCzd9H8batsPMq5/thumbnail-1280x720_1.png',
+        time: '00:38'
       },
       {
-        title: 'Around the Block - Trailer',
-        url: '/play/a4Dbd26pLu0X',
-        image: '/assets/img/landing/small/around-the-block-trailer.png',
-        time: '02:56'
+        title: 'A Conversation with Naval Ravikant and Ryan Shea | Blockstack Summit 2017 ',
+        url: '/play/p1TyceTSWr8p',
+        image: 'https://gateway.paratii.video/ipfs/QmRutqHVzdyDFc2UZT3nmu4aeqdBCwG2fxmqiXBz5GQjPx/thumbnail-1280x720_1.png',
+        time: '31:02'
       },
       {
-        title: 'Todo Tempo do Mundo - Ep. 1',
-        url: '/play/mF7YHwBeGqZq',
-        image: '/assets/img/landing/small/todo-tempo-do-mundo-ep-1.png',
-        time: '18:38'
+        title: 'ETH Buenos Aires | The ETHCommunity Speaks: Chris Burniske ',
+        url: '/play/On1UzFx05tk4',
+        image: 'https://gateway.paratii.video/ipfs/QmZL2Pe4K62GjtzHgDgHdJG3eTRigdgxzcLqQBWVLiQEqc/thumbnail-1280x720_1.png',
+        time: '02:15'
       },
       {
-        title: 'Só Quero Fazer Falta',
-        url: '/play/9Jh8KlGxtRIC',
-        image: '/assets/img/landing/small/so-quero-fazer-falta.png',
-        time: '02:03'
+        title: 'ETHBuenosAires 2018: The final cut',
+        url: '/play/yFnPGUOvLZm8',
+        image: 'https://gateway.paratii.video/ipfs/QmYvaVc8qaFegRNZRzQ1r2yDm1sNpjPhP1d9VpvPoZ3FKY/thumbnail-1280x720_1.png',
+        time: '03:09'
       }
     ]
   },
@@ -115,27 +115,27 @@ export const LANDING_CATEGORY_VIDEOS: Array<Object> = [
     slug: '/category/network-builders',
     list: [
       {
-        title: 'O que é Algoritmo',
-        url: '/play/D93ftQHK3OXN',
-        image: '/assets/img/landing/small/o-que-e-algoritimo.png',
-        time: '02:48'
+        title: 'Blockstack - A New Internet for Decentralized Apps',
+        url: 'play/v42pJ9zBpoXQ',
+        image: 'https://gateway.paratii.video/ipfs/QmWZEVvJDrnanBGsi9kcNELHjnCq1nkTeoTg8RK2UUx32j/thumbnail-1280x552_1.png',
+        time: '05:35'
       },
       {
-        title: 'Around the Block - Trailer',
-        url: '/play/a4Dbd26pLu0X',
-        image: '/assets/img/landing/small/around-the-block-trailer.png',
-        time: '02:56'
+        title: 'Aragon - The Fight for Freedom',
+        url: 'play/qgdKBMkcEMpv',
+        image: 'https://gateway.paratii.video/ipfs/QmVDTceGNnahnheXNXTJLxEhiUTSCUyZH4fb4s4vV6WE5R/thumbnail-1280x464_1.png',
+        time: '05:50'
       },
       {
-        title: 'Todo Tempo do Mundo - Ep. 1',
-        url: '/play/mF7YHwBeGqZq',
-        image: '/assets/img/landing/small/todo-tempo-do-mundo-ep-1.png',
-        time: '18:38'
+        title: 'Introducing DAOstack',
+        url: 'play/e5Md7X3NRXuS',
+        image: 'https://gateway.paratii.video/ipfs/QmPhS5uW4ZXzSRt4AuhioigDHR3d9WDc7JS9nPnao6ayhh/thumbnail-1280x534_1.png',
+        time: '05.16'
       },
       {
-        title: 'Só Quero Fazer Falta',
-        url: '/play/9Jh8KlGxtRIC',
-        image: '/assets/img/landing/small/so-quero-fazer-falta.png',
+        title: 'Radar Relay | Modular Trade Networks ',
+        url: 'play/dkxn1c6UCg9N',
+        image: 'so-quero-fazer-falta.png',
         time: '02:03'
       }
     ]
@@ -145,7 +145,7 @@ export const LANDING_CATEGORY_VIDEOS: Array<Object> = [
     slug: '/category/educational',
     list: [
       {
-        title: 'O que é Algoritmo',
+        title: 'The Value Revolution: How Blockchain Will Change Money & the World | Galia Benartzi | TEDxWhiteCity',
         url: '/play/D93ftQHK3OXN',
         image: '/assets/img/landing/small/o-que-e-algoritimo.png',
         time: '02:48'
